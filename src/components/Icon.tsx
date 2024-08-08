afc7a41c3814d956e3aa05cfb616071a60196fb1
hyunjun9788 <namhyunjun9788@gmail.com>
2024-08-08 17:56:32 +0900

    #1 feat: Icon 공통 컴포넌트 구현

---
+++ src/components/Icon.tsx
@@ -0,0 +1,27 @@
+import { IconProps, IconType } from '@/types/iconType';
+import HomeIcon from '../../public/house-solid.svg';
+import NoticeIcon from '../../public/comment-solid.svg';
+import SearchIcon from '../../public/magnifying-glass-solid.svg';
+import MusicIcon from '../../public/music-solid.svg';
+import ReviewIcon from '../../public/pen-solid.svg';
+import MusicalIcon from '../../public/people-pulling-solid.svg';
+import PlayIcon from '../../public/people-roof-solid.svg';
+
+export const iconTypes: Record<
+  IconType,
+  React.ComponentType<React.SVGProps<SVGSVGElement>>
+> = {
+  HomeIcon,
+  ReviewIcon,
+  NoticeIcon,
+  MusicIcon,
+  SearchIcon,
+  PlayIcon,
+  MusicalIcon,
+};
+
+export const Icon = ({ name, className, onClick }: IconProps) => {
+  const IconComponent = iconTypes[name];
+
+  return <IconComponent className={className} onClick={onClick} />;
+};
---
+++ src/types/iconType.ts
@@ -0,0 +1,21 @@
+import { HTMLAttributes } from 'react';
+
+export type IconType =
+  | 'HomeIcon'
+  | 'ReviewIcon'
+  | 'NoticeIcon'
+  | 'MusicIcon'
+  | 'SearchIcon'
+  | 'PlayIcon'
+  | 'MusicalIcon';
+
+export interface IconProps {
+  name: IconType;
+  className: HTMLAttributes<SVGAElement>['className'];
+  onClick?: React.MouseEventHandler<SVGSVGElement>;
+}
+
+export interface LoadingProps {
+  iconClassName?: HTMLAttributes<SVGAElement>['className'];
+  textClassName?: HTMLAttributes<HTMLParagraphElement>['className'];
+}
