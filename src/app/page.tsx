9aad9a416dce68a508d260af1dc4ff77bff92abc
hyunjun9788 <namhyunjun9788@gmail.com>
2024-08-08 14:30:28 +0900

    #1 feat: Gnb 로그인 버튼 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="text-main">안녕하세요</div>;
+  return <></>;
 }
--- src/components/Gnb.tsx
+++ src/components/Gnb.tsx
@@ -1,9 +1,22 @@
+import Link from 'next/link';
 import Logo from '../../public/logo.svg';
 
 function Gnb() {
   return (
-    <header className="h-14 border-b border-gray-36 ">
-      <Logo className="w-[110px] h-[45px]" />
+    <header className="fixed top-0 left-0 w-full h-18 border-b border-gray-36 z-[999]">
+      <div className="flex items-center justify-between px-8 py-4">
+        <div className="flex-1 flex justify-center">
+          <Link href="/">
+            <Logo className="w-[130px] h-[55px]" />
+          </Link>
+        </div>
+        <button
+          type="button"
+          className="px-4 py-[6px] border border-white rounded-lg text-white"
+        >
+          로그인
+        </button>
+      </div>
     </header>
   );
 }
