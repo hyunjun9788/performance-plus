54ff99e2faf755032ed8b1433cebad7551f454d8
hyunjun9788 <namhyunjun9788@gmail.com>
2024-08-09 23:34:21 +0900

    #2 feat: 홈화면 배너 UI 1페이지 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <></>;
+import Banner from '@/components/Banner';
+
+function Home() {
+  return (
+    <main>
+      <Banner />
+    </main>
+  );
 }
+
+export default Home;
